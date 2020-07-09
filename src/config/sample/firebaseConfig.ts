0a5200a0f798cffbbf936bef56c77bcc70fbf4f7
大崎 貴行 <osaki@ipl-soft.co.jp>
2020-07-09 11:44:37 +0900

    init

---
+++ src/config/sample/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: 'test',
+    authDomain: 'test',
+    databaseURL: 'test',
+    projectId: 'test',
+    storageBucket: 'test',
+    messagingSenderId: 'test',
+    appId: 'test',
+    measurementId: 'test',
+};
